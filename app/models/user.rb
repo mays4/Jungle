96fcb50f8b1554390df1b08337b5ca25bc0b1116
mays4 <maysalani84@yahoo.com>
2021-11-16 21:31:58 -0500

    changed the login and signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,20 @@
 class User < ActiveRecord::Base
-  def  self.authenticate_with_credentials (email,password)
-     email = email.downcase.strip
-      user = User.find_by(email: email) 
-        if user && user.authenticate(password)
-          user
-        else
-          nil 
-       end
-  end
-  has_secure_password
-  validates :name, presence: true
-  validates :password , presence: true ,length: { minimum: 6 }
-  validates :password_confirmation, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true, uniqueness:{ case_sensitive: false }
 
+has_secure_password
+validates :name, presence: true
+validates :password , presence: true ,length: { minimum: 6 }
+validates :password_confirmation, presence: true
+validates :last_name, presence: true
+validates :email, presence: true, uniqueness:{ case_sensitive: false }
+
+
+   def  self.authenticate_with_credentials (email,password)
+    email = email.downcase.strip
+    user = User.find_by(email: email) 
+      if user && user.authenticate(password)
+        user
+      else
+        nil 
+     end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20211115025652) do
 ActiveRecord::Schema.define(version: 20211115025652) do
 
   # These are extensions that must be enabled in order to support this database
@@ -58,7 +56,6 @@ ActiveRecord::Schema.define(version: 20211115025652) do
 
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
 
-
   create_table "sales", force: :cascade do |t|
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
@@ -67,7 +64,6 @@ ActiveRecord::Schema.define(version: 20211115025652) do
     t.date     "ends_on"
     t.string   "name"
   end
-
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
@@ -81,5 +77,4 @@ ActiveRecord::Schema.define(version: 20211115025652) do
   add_foreign_key "line_items", "orders"
   add_foreign_key "line_items", "products"
   add_foreign_key "products", "categories"
-end
 end
